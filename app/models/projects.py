20732e475e696864fee7c40b405beccc58a1c991
zhanglinshuai <1216486490@qq.com>
2026-06-08 09:16:25 +0800

    完成项目表的数据库字段的定义

---
+++ app/models/projects.py
@@ -0,0 +1,21 @@
+﻿from datetime import datetime
+
+from sqlalchemy import String, Text, Integer, DateTime, ForeignKey, func, Boolean
+from sqlalchemy.orm import mapped_column, Mapped
+
+from app.database import Base
+
+
+class Project(Base):
+    __tablename__ = "project"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    description: Mapped[Text | None] = mapped_column(Text, nullable=True)
+    repo_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    default_env_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    created_by: Mapped[int] = mapped_column(Integer, nullable=False)
+    updated_by: Mapped[int] = mapped_column(Integer, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(),
+                                                 onupdate=func.now())
--- sql/create_sql.sql
+++ sql/create_sql.sql
@@ -1,3 +1,6 @@
+/**
+用户表的建表语句
+ */
 -- auto-generated definition
 create table "user"
 (
@@ -34,5 +37,52 @@ comment on column "user".created_at is '创建时间';
 comment on column "user".updated_at is '更新时间';
 
 alter table "user"
+    owner to postgres;
+
+/**
+项目表的建表语句
+ */
+-- auto-generated definition
+create table project
+(
+    id             integer                 not null
+        constraint project_pk
+            primary key,
+    name           varchar(50)
+        constraint project_pk_2
+            unique,
+    description    text,
+    repo_url       varchar(500)            not null,
+    default_env_id integer                 not null,
+    created_id     integer                 not null,
+    updated_id     integer                 not null,
+    is_active      boolean   default true  not null,
+    created_at     timestamp default now() not null,
+    updated_at     timestamp default now() not null
+);
+
+comment on table project is '项目表';
+
+comment on column project.id is '自增主键';
+
+comment on column project.name is '项目名称';
+
+comment on column project.description is '项目描述';
+
+comment on column project.repo_url is '代码仓库地址';
+
+comment on column project.default_env_id is '默认环境id';
+
+comment on column project.created_id is '创建人id';
+
+comment on column project.updated_id is '最后修改人id';
+
+comment on column project.is_active is '是否启用';
+
+comment on column project.created_at is '创建时间';
+
+comment on column project.updated_at is '最后更新时间';
+
+alter table project
     owner to postgres;
 
